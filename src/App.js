5eff10ac3f1ec8052d9974786bf6f1b91042322b
Thaddious Gorges <thaddiousgorges@Thaddiouss-MBP.hsd1.mi.comcast.net>
2020-10-06 04:49:24 -0400

    added bootstrap and basic navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
 import Home from './components/Home/Home';
+import Signup from './components/Register/Signup';
+import Login from './components/Register/Login';
 import './App.css';
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
 
           {/* Home is default */}
           <Route path="/" exact component={Home} />
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/login" exact component={Login} />
 
         </Switch>
       </div>
