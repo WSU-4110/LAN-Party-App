e4924d1dad3fe1569cd0b5e511339570e0f2ea47
ga2599 <ga2599@wayne.edu>
2020-10-05 21:25:18 -0400

    use the database.js to connect to AWS

--- database.js
+++ database.js
@@ -1,7 +1,34 @@
+// Connecting to the database
+
 const mongoose = require('mongoose')
 
-const connection = "mongodb+srv://username:<password>@<cluster>/<database>?retryWrites=true&w=majority"
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
 
-mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+const password = process.argv[2]
+const url = `mongodb+srv://logan:${password}@cluster0.pgumn.mongodb.net/test?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log(err))
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is Easy',
+  date: new Date(),
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-// The entry point to the backend
+// This the entry point to the entire backend
 
 const express = require('express')
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path')
-require('./database');
+//const bodyParser = require('body-parser');
+//const cors = require('cors');
+//const path = require('path')
+//require('./database');
 
 const app = express();
 
-app.use(bodyParser.json())
-app.use(cors());
+//app.use(bodyParser.json())
+//app.use(cors());
 app.use(express.json())
 
 // API
-const user = rquire('/api/users')
+//const user = require('/api/users')
 
-app.use('/api/users', users)
-app.use(express.static(path.join(__dirname, '../build')))
+//app.use('/api/users', users)
+//app.use(express.static(path.join(__dirname, '../build')))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
-})
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../build'))
+// })
 
 const port = process.env.PORT || 5000;
 
