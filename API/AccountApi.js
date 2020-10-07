80b19e4d622502937519955643147f547bb8c3b2
James Bradbury <fe4034@wayne.edu>
2020-10-07 19:02:31 -0400

    added signin endpoint

--- API/AccountApi.js
+++ API/AccountApi.js
@@ -113,8 +113,78 @@ module.exports = {
 //************************************************************** */
 
 
-signin : async function (evt) {
+signin : async function (event) {
   
+
+  try {
+    //event --> carrys all the post parameters were sendiong to this end point ex : event.body.firstName, event.body.LastName, etc...
+    if (!event) {
+      //if nothing was provided in the request, return a 204 HTTPS code (No content)
+      return {
+        statusCode: 204,
+        message: JSON.stringify("No information was provided with the request"),
+      };
+    }
+
+    //Next, if there was data provided in the request
+    const request_data = JSON.parse(event.body); // parse the data sent to this request (less syntax when we want to access it , ex : request_data.firstName instead of event.body.firstName)
+
+    // THEN validate it and verify its credible for our database to accomodate (Using Joi)
+
+
+    request_data.email = request_data.email.toLowerCase()
+
+    //next we want to check if this user already has an account with us by using the email GSI we set up in the resources section for the users table (in the serverless.yml file)
+
+    const get_user_by_email = {
+      TableName: "users", //table we want to query
+      Key: {email : request_data.email}, //Key  want to query by
+    };
+
+
+
+    //Run the query
+    const is_exsisting = await dynamoDB
+      .get(get_user_by_email)
+      .promise();
+
+    //if a result was returned and there was atleast 1 record with this email,
+    if (!is_exsisting && !is_exsisting.Item ) {
+      return {
+        statusCode: 403,
+        body: JSON.stringify("Error : Email not registered - Register for a new acount"),
+      };
+    }
+
+
+    if (is_exsisting.Item.password === request_data.password){
+
+      return {
+        // new user was successfully created //
+        statusCode: 200,
+        body: JSON.stringify(is_exsisting.Item), //return data
+      };
+
+    }
+    else {
+
+      return {
+        // new user was successfully created //
+        statusCode: 200,
+        body: JSON.stringify("Invalid password"), //return data
+      };
+    }
+
+
+ 
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error), //return error
+    };
+  }
+
 
 }
 
