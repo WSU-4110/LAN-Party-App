9ddf4f47070a81ab405da88f5af1339042aba05a
ThaddG <thaddious.gorges@gmail.com>
2020-10-10 17:01:11 -0400

    added host party component

--- src/components/Register/Login.js
+++ src/components/Register/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useForm } from "react-hook-form";
 import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
+import './Register.css'
 
 const Login = () => {
   const { REACT_APP_URL } = process.env;
@@ -28,36 +29,44 @@ const Login = () => {
   }
 
   return(
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <Form.Group controlId="formEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control 
-          type="email" 
-          placeholder="Enter email" 
-          name="email"
-          aria-describedby="emailReq"
-          ref={register({ required: true })} />
-        {errors.email && <Form.Text className="text-danger" id="emailReq">Required</Form.Text>}
-      </Form.Group>
+    <div className="form-container">
+      <div className="form-content">
+        <div className="component-header">
+          <h2>Login</h2>
+        </div>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group controlId="formEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control 
+                type="email" 
+                placeholder="Enter email" 
+                name="email"
+                aria-describedby="emailReq"
+                ref={register({ required: true })} />
+              {errors.email && <Form.Text className="text-danger" id="emailReq">Required</Form.Text>}
+            </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control 
-          type="password" 
-          placeholder="Password"
-          name="password"
-          aria-describedby="passwordReq"
-          ref={register({ required: true, minLength: 8 })} />
-          {errors.password && errors.password.type === "required" && (
-            <Form.Text className="text-danger" id="passwordReq">Required</Form.Text>)}
-          {errors.password && errors.password.type === "minLength" && (
-            <Form.Text className="text-danger" id="passwordReq">Minimum 8 characters</Form.Text>)}
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control 
+                type="password" 
+                placeholder="Password"
+                name="password"
+                aria-describedby="passwordReq"
+                ref={register({ required: true, minLength: 8 })} />
+                {errors.password && errors.password.type === "required" && (
+                  <Form.Text className="text-danger" id="passwordReq">Required</Form.Text>)}
+                {errors.password && errors.password.type === "minLength" && (
+                  <Form.Text className="text-danger" id="passwordReq">Minimum 8 characters</Form.Text>)}
+            </Form.Group>
+            <div style={{textAlign:"center"}}>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </div>
+          </Form>
+      </div>
+    </div>
   )
 }
 
--- src/components/Register/Signup.js
+++ src/components/Register/Signup.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useForm } from "react-hook-form";
 import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
+import './Register.css';
 
 const Signup = () => {
   const { REACT_APP_URL } = process.env;
@@ -10,10 +11,9 @@ const Signup = () => {
   const onSubmit = (data) => {
     // This is the object we're sending through
     const payload = {
-      first_name: data.firstName,
-      last_name: data.lastName,
-      password: data.password,
-      email: data.email,
+      Username: data.username,
+      Password: data.password,
+      Email: data.email,
     };
 
     const headers = {
@@ -31,74 +31,72 @@ const Signup = () => {
   }
 
   return(
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <Form.Group controlId="formFirstName">
-        <Form.Label>First Name</Form.Label>
-        <Form.Control 
-          type="text" 
-          placeholder="Spongebob"
-          name="firstName"
-          aria-describedby="firstNameReq"
-          ref={register({ required: true })} />
-        {errors.firstName && <Form.Text className="text-danger" id="firstNameReq">Required</Form.Text>}
-      </Form.Group>
+    <div className="form-container">
+      <div className="form-content">
+        <div className="component-header">
+          <h2>Sign Up</h2>
+        </div>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control 
+              type="email" 
+              placeholder="Enter email" 
+              name="email"
+              aria-describedby="emailReq"
+              ref={register({ required: true })} />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+            {errors.email && <Form.Text className="text-danger" id="emailReq">Required</Form.Text>}
+          </Form.Group>
 
-      <Form.Group controlId="formLastName">
-        <Form.Label>Last Name</Form.Label>
-        <Form.Control 
-          type="text" 
-          placeholder="Squarepants"
-          name="lastName"
-          aria-describedby="lastNameReq"
-          ref={register({ required: true })} />
-        {errors.lastName && <Form.Text className="text-danger" id="lastNameReq">Required</Form.Text>}
-      </Form.Group>
+          <Form.Group controlId="formFirstName">
+            <Form.Label>Username</Form.Label>
+            <Form.Control 
+              type="text" 
+              placeholder="Spongebob"
+              name="username"
+              aria-describedby="usernameReq"
+              ref={register({ required: true })} />
+            {errors.username && <Form.Text className="text-danger" id="usernameReq">Required</Form.Text>}
+          </Form.Group>
 
-      <Form.Group controlId="formEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control 
-          type="email" 
-          placeholder="Enter email" 
-          name="email"
-          aria-describedby="emailReq"
-          ref={register({ required: true })} />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-        {errors.email && <Form.Text className="text-danger" id="emailReq">Required</Form.Text>}
-      </Form.Group>
+          {/* <Form.Group controlId="formAddress">
+            <Form.Label>Address</Form.Label>
+            <Form.Control 
+              type="text" 
+              placeholder="12345 Gorges St"
+              name="address"
+              aria-describedby="addressReq"
+              ref={register({ required: true })} />
+              <Form.Text className="text-muted">
+                We'll never share your address with anyone else.
+              </Form.Text>
+              {errors.address && <Form.Text className="text-danger" id="addressReq">Required</Form.Text>}
+          </Form.Group> */}
 
-      {/* <Form.Group controlId="formAddress">
-        <Form.Label>Address</Form.Label>
-        <Form.Control 
-          type="text" 
-          placeholder="12345 Gorges St"
-          name="address"
-          aria-describedby="addressReq"
-          ref={register({ required: true })} />
-          <Form.Text className="text-muted">
-            We'll never share your address with anyone else.
-          </Form.Text>
-          {errors.address && <Form.Text className="text-danger" id="addressReq">Required</Form.Text>}
-      </Form.Group> */}
-
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control 
-          type="password" 
-          placeholder="Password"
-          name="password"
-          aria-describedby="passwordReq"
-          ref={register({ required: true, minLength: 8 })} />
-          {errors.password && errors.password.type === "required" && (
-            <Form.Text className="text-danger" id="passwordReq">Required</Form.Text>)}
-          {errors.password && errors.password.type === "minLength" && (
-            <Form.Text className="text-danger" id="passwordReq">Minimum 8 characters</Form.Text>)}
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control 
+              type="password" 
+              placeholder="Password"
+              name="password"
+              aria-describedby="passwordReq"
+              ref={register({ required: true, minLength: 8 })} />
+              {errors.password && errors.password.type === "required" && (
+                <Form.Text className="text-danger" id="passwordReq">Required</Form.Text>)}
+              {errors.password && errors.password.type === "minLength" && (
+                <Form.Text className="text-danger" id="passwordReq">Minimum 8 characters</Form.Text>)}
+          </Form.Group>
+          <div style={{textAlign:"center"}}>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </div>
   )
 }
 
