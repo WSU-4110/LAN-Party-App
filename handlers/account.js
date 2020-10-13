0ac3befe906539083f8b19f8c703dfd0763f8b8e
ga2599 <ga2599@wayne.edu>
2020-10-12 21:22:59 -0400

    view an account finished

--- handlers/account.js
+++ handlers/account.js
@@ -16,7 +16,7 @@ module.exports = {
 
             // next parse the request
             let request = JSON.parse(event.body);
-            
+
             // they need to have entered an email, username, and a password
             if (!request.Email)
                 throw new Error("Account Email Required!");
@@ -28,14 +28,14 @@ module.exports = {
                 throw new Error("Account Password Required!");
 
             // change the email to lowercase
-            request.Email = request.Email.toLowerCase();            
+            request.Email = request.Email.toLowerCase();
 
             // let's have a create date for our new account
             request.CreateDate = moment().toISOString();
-        
+
             // let's check and see if the user already has an account
             let exists = await AccountAPI.GetByEmail(request.Email);
-            
+
             // if they don't already have an account then we can make them one
             if (!exists) {
                 let AccountID = request.ID ? request.ID : false;
@@ -45,7 +45,7 @@ module.exports = {
                     Message: "Account created, log in to continue!",
                     Account: request
                 };
-                
+
                 return responseUtil.Build(200, result);
             } else throw new Error("Email Already Exists!");
         } catch (err) {
@@ -60,7 +60,7 @@ module.exports = {
             // if nothing was provided in the request, return a 204 HTTPS code (No content)
             if (!event)
                 return responseUtil.Build(204, { Message: "No information was provided with the request!" });
-            
+
             // next parse the request
             let request = JSON.parse(event.body);
 
@@ -72,32 +72,30 @@ module.exports = {
                 throw new Error("Account Password Required!");
 
             // change the email to lowercase
-            request.Email = request.Email.toLowerCase(); 
-            
+            request.Email = request.Email.toLowerCase();
+
             // authenticate the account
             let account = await AccountAPI.AuthByEmailPassword(request.Email, request.Password);
-            
+
             // if we returned with success, then save
             if (account.ID)
                 return responseUtil.Build(200, account);
-            
+
             // else, return an error
             return responseUtil.Build(500, { Message: "Account Login Error Detected." });
         } catch (err) {
-          console.error("Login Error:", err);
-          return responseUtil.Build(500, { Message: err.message });
+            console.error("Login Error:", err);
+            return responseUtil.Build(500, { Message: err.message });
         }
     },
 
     // VIEW AN ACCOUNT //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     View: async function (event) {
         try {
-            // if nothing was provided in the request, return a 204 HTTPS code (No content)
-            if (!event)
-                return responseUtil.Build(204, { Message: "No information was provided with the request!" });
-            
-            // next parse the request
-            let request = JSON.parse(event.body);
+            // grab the ID
+            let request = {
+                ID: event.pathParameters.ID
+            };
 
             // we need to make sure that we were give an ID
             if (!request.ID)
@@ -105,7 +103,7 @@ module.exports = {
 
             // return the account information
             let account = await AccountAPI.Get(request.ID);
-            
+
             // if we returned with success, then create a result object
             if (account.ID) {
                 let result = {
@@ -115,8 +113,8 @@ module.exports = {
                 return responseUtil.Build(200, result); // send the result
             } else return responseUtil.Build(500, { Message: "Account Login Error Detected." }); // else, return an error
         } catch (err) {
-          console.error("Login Error:", err);
-          return responseUtil.Build(500, { Message: err.message });
+            console.error("Login Error:", err);
+            return responseUtil.Build(500, { Message: err.message });
         }
     }
 };
