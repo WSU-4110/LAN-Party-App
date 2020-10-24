6a4519cf06c12966a13ff1119e706e1e81d502b2
Jarred Crull <gj3842@wayne.edu>
2020-10-24 10:33:30 -0400

    Replaced regex with simpler function.

--- utilities/nameCheck.js
+++ utilities/nameCheck.js
@@ -6,15 +6,13 @@ module.exports = {
 
     //Check if a name is valid
     isValidParty: function (name){
-        //Check if there are any non-whitespace characters in the start
-        let nonWhitespace = /\S/;
-        if(nonWhitespace.test(name) === null){
-            //Name is all whitespace. Not valid
+        //Trim the string of whitespace 
+        name = name.trim();
+
+        //If the name was empty, return false
+        if(name === ''){
             return false;
         }
-        
-        //The first character must be non-whitespace
-        name = name.substr(nonWhitespace.lastIndex - 1);
 
         //Replace multiple neighboring whitespaces with single spaces
         name = name.split(/\s\s+/).join(' ');
