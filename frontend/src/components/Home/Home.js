5334c440f819ddebed69a085c83137280f7b86b2
James Bradbury <fe4034@wayne.edu>
2020-10-31 21:08:33 -0400

    Work in progress on the home page, but need to merge first to get Thadd's changes

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,13 +1,60 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, FormControl } from 'react';
 import { NavLink } from 'react-router-dom';
 import Map from '../GoogleMap/GoogleMap';
 import cookies from 'js-cookie';
-import {Button, Accordion, Card} from 'react-bootstrap';
+import {Button, Accordion, Card, Dropdown} from 'react-bootstrap';
 import { UserContext } from '../../UserContext';
 import { PartiesContext } from '../../PartiesContext'
 import { HomeRenderContext } from '../../HomeRenderContext'
 import ViewParty from '../ViewParty/ViewParty';
 import axios from 'axios';
+
+
+const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+  <a
+    href=""
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  >
+    {children}
+    &#x25bc;
+  </a>
+));
+
+// forwardRef again here!
+// Dropdown needs access to the DOM of the Menu to measure it
+const CustomMenu = React.forwardRef(
+  ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+    const [value, setValue] = useState('');
+
+    return (
+      <div
+        ref={ref}
+        style={style}
+        className={className}
+        aria-labelledby={labeledBy}
+      >
+        <FormControl
+          autoFocus
+          className="mx-3 my-2 w-auto"
+          placeholder="Type to filter..."
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+        />
+        <ul className="list-unstyled">
+          {React.Children.toArray(children).filter(
+            (child) =>
+              !value || child.props.children.toLowerCase().startsWith(value),
+          )}
+        </ul>
+      </div>
+    );
+  },
+);
+
 
 
 
@@ -50,6 +97,9 @@ const Home = (props) => {
 
 
   
+
+
+  
   return(
     <div>
       <Map />
@@ -71,6 +121,43 @@ const Home = (props) => {
         <Button variant="info" size="lg" onClick={toLogin}>Host A Party</Button>
       </div>
       }
+
+
+<Dropdown>
+        Custom toggle
+      <Dropdown.Menu as={CustomMenu}>
+        {parties.map((p, i) => (
+          <Card>
+            <Dropdown.Toggle
+            style={{
+              padding: "10px 10px 5px",
+              borderBottom:"2px solid #0C0C0D",
+              backgroundColor: "#35373D"
+            }} 
+            as={Card.Header} 
+            eventKey={p.ID}>
+              {p.Name} <br/>
+              Host: {p.HostUsername} <br/>
+              Location: {p.Location} <br/>
+              Date: {p.Date} <br/>
+              </Dropdown.Toggle>
+            <Dropdown.Item eventKey={p.ID}>
+              <Card.Body>
+                <ViewParty 
+                location={p.Location} 
+                name={p.Name}
+                host={p.HostUsername} 
+                date={p.Date} />
+              </Card.Body>
+            </Dropdown.Item>
+           </Card>
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>,
+
+
+
+
       <Accordion>
         {parties.map((p, i) => (
           <Card>
