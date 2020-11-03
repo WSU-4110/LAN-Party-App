b771b40600ea2ebeee350135973f5d9b1776573d
James Bradbury <fe4034@wayne.edu>
2020-11-03 00:19:51 -0500

    Took out the drop down, I broke it, then I gave up on implementing search (for now) just can't get it to work with the current UI without butchering it.

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,61 +2,15 @@ import React, { useState, useEffect, useContext, FormControl } from 'react';
 import { NavLink } from 'react-router-dom';
 import Map from '../GoogleMap/GoogleMap';
 import cookies from 'js-cookie';
-import {Button, Accordion, Card} from 'react-bootstrap';
+import {Button, Accordion, Card, Dropdown} from 'react-bootstrap';
 import { UserContext } from '../../context/UserContext';
 import { PartiesContext } from '../../context/PartiesContext'
 import { HomeRenderContext } from '../../context/HomeRenderContext'
 import ViewParty from '../ViewParty/ViewParty';
 import axios from 'axios';
 
-
-const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
-  <a
-    href=""
-    ref={ref}
-    onClick={(e) => {
-      e.preventDefault();
-      onClick(e);
-    }}
-  >
-    {children}
-    &#x25bc;
-  </a>
-));
-
-// forwardRef again here!
-// Dropdown needs access to the DOM of the Menu to measure it
-const CustomMenu = React.forwardRef(
-  ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
-    const [value, setValue] = useState('');
-
-    return (
-      <div
-        ref={ref}
-        style={style}
-        className={className}
-        aria-labelledby={labeledBy}
-      >
-        <FormControl
-          autoFocus
-          className="mx-3 my-2 w-auto"
-          placeholder="Type to filter..."
-          onChange={(e) => setValue(e.target.value)}
-          value={value}
-        />
-        <ul className="list-unstyled">
-          {React.Children.toArray(children).filter(
-            (child) =>
-              !value || child.props.children.toLowerCase().startsWith(value),
-          )}
-        </ul>
-      </div>
-    );
-  },
-);
-
-
-
+// RIP CustomToggle, will try again to use it for search, but I failed for sprint 2
+// -James
 
 const Home = (props) => {
   const { REACT_APP_URL } = process.env;
@@ -96,8 +50,6 @@ const Home = (props) => {
   } , [homeRender])
 
 
-  
-
 
   
   return(
@@ -121,41 +73,6 @@ const Home = (props) => {
         <Button variant="info" size="lg" onClick={toLogin}>Host A Party</Button>
       </div>
       }
-
-
-<Dropdown>
-        Custom toggle
-      <Dropdown.Menu as={CustomMenu}>
-        {parties.map((p, i) => (
-          <Card>
-            <Dropdown.Toggle
-            style={{
-              padding: "10px 10px 5px",
-              borderBottom:"2px solid #0C0C0D",
-              backgroundColor: "#35373D"
-            }} 
-            as={Card.Header} 
-            eventKey={p.ID}>
-              {p.Name} <br/>
-              Host: {p.HostUsername} <br/>
-              Location: {p.Location} <br/>
-              Date: {p.Date} <br/>
-              </Dropdown.Toggle>
-            <Dropdown.Item eventKey={p.ID}>
-              <Card.Body>
-                <ViewParty 
-                location={p.Location} 
-                name={p.Name}
-                host={p.HostUsername} 
-                date={p.Date} />
-              </Card.Body>
-            </Dropdown.Item>
-           </Card>
-        ))}
-      </Dropdown.Menu>
-    </Dropdown>,
-
-
 
 
       <Accordion>
