fb2496ff3d460ae2b958b65fe900f5d0cee1afd9
ga2599 <ga2599@wayne.edu>
2020-11-11 19:16:02 -0500

    get game by ID handler

--- backend/handlers/game.js
+++ backend/handlers/game.js
@@ -7,8 +7,32 @@ const responseUtil = require("../utilities/response");
 module.exports = {
         
     // VIEW A GAME //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ViewGame: async function () {
+    ViewGame: async function (event) {
+        try {
+            // grab the ID
+            let request = {
+                ID: event.pathParameters.ID
+            };
 
+            // we need to make sure that we were give an ID
+            if (!request.ID)
+                throw new Error("Game ID Required!");
+
+            // return the account information
+            let game = await GameAPI.Get(request.ID);
+
+            // if we returned with success, then create a result object
+            if (game.ID) {
+                let result = {
+                    Message: "Returning the game!",
+                    Game: game
+                };
+                return responseUtil.Build(200, result); // send the result
+            } else return responseUtil.Build(500, { Message: "No Game With That ID." }); // else, return an error
+        } catch (err) {
+            console.log(err);
+            return responseUtil.Build(500, { Message: err.message });
+        }
     },
 
     // VIEW THE ENTIRE LIST OF GAMES //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
