7cff16354c1ccb2d54ba19a58ba4149495413c30
ga2599 <ga2599@wayne.edu>
2020-11-11 19:17:50 -0500

    get game by id API function

--- backend/services/GameAPI.js
+++ backend/services/GameAPI.js
@@ -1,18 +1,34 @@
 'use strict';
 
 // Imports
-const crypto = require("crypto");
 const shortid = require("shortid");
 const AWS = require("aws-sdk");
 
 // Updating AWS settings
 AWS.config.update({ region: "us-east-2" }); // region
-let tableName = 'GAMES-LAN'; // the name of our account table in the AWS database
+const tableName = 'GAMES-LAN'; // the name of our game table in the AWS database
 
 module.exports = {
 
-  // RETURN A GAME //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  Get: async function (ID, AccountInfo) {
+  // RETURN A GAME BY ID //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  Get: async function (ID) {
+    try {
+      let dynamoDB = new AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10" }); // connect to the database
+
+      // create the parameters for the query
+      let params = {
+        TableName: tableName,
+        Key: { ID: ID },
+      };
+
+      let result = await dynamoDB.get(params).promise(); // grab the game
+
+      if (result.Item) return result.Item;
+      else return false;
+    } catch (err) {
+      console.log(err.message);
+      throw new Error("Get Game Error");
+    }
   },
 
   // RETURN ALL GAMES //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
