3566ddc5b91a9cf9c4d111c862a35afa81d4edd4
Jarred Crull <gj3842@wayne.edu>
2020-11-14 21:55:45 -0500

    Init value check function to simplify verification

--- backend/utilities/PartyCheck.js
+++ backend/utilities/PartyCheck.js
@@ -1,6 +1,84 @@
 'use strict';
 
+const moment = require("moment-timezone");
+const AccountAPI = require("../services/AccountAPI");
+
 module.exports = {
+    validPartyKeys: function(key, value, context){
+        output = {};
+        
+        switch (key) {
+            case PartyName:
+                output.value = this.isValidParty(value);
+                output.isValid = (output.value !== false);
+                return output;
+
+            case PartyLocation:
+                let reqs = ['Longitude', 'Latitude', 'Name'];
+
+                let missingKey = false;
+
+                reqs.forEach(key => {
+                    if(!value.hasOwnProperty(key)){
+                        missingKey = key;
+                    } else {
+                     output.value[key] = value[key];
+                    }
+                })
+
+                output.isValid = (missingKey !== false);
+                return output;
+
+            case Host:
+                let account;
+                try {
+                    account = await AccountAPI.Get(value);
+                    if (account === false){
+                        output.isValid = false;
+                        return output;
+                    }
+                } catch (err) {
+                    output.isValid = false;
+                        return output;
+                }
+
+                output.value = {
+                    Host: account.ID,
+                    HostUsername: account.Username
+                };
+                output.isValid = true;
+                return output;
+
+            case PartyTime:
+                output.value = value;
+                try {
+                output.isValid = value.isAfter(moment.now());
+                } catch (err){
+                    output.isValid = false;
+                }
+                return output;
+
+            case AgeGate:
+                output.value = value;
+                output.isValid = (typeof value === Boolean)
+                return output;
+
+            case Intent:
+                output.value = value;
+                output.isValid = (value === "Casual" || value === "Competative");
+                return output;
+
+            case RequestLocationChange:
+                output.value = null;
+                output.isValid = true;
+                return output;
+
+            default:
+                output.value = value;
+                output.isValid = true;
+                return output;
+        }
+    },
 
     //Check if a name is valid
     isValidParty: function (name){
