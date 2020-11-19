33ad6a095346edac8ca093dd4b3c000a9f43f896
Jarred Crull <gj3842@wayne.edu>
2020-11-19 15:02:51 -0500

    Created insertSort function.

--- backend/utilities/PartyCheck.js
+++ backend/utilities/PartyCheck.js
@@ -132,17 +132,60 @@ module.exports = {
         //Make sure that the list isn't empty
         if(list === undefined || list.length === 0){
             list = [insertItem];
+            return list;
         }
         
         //If the first item is greater than the new item, put the item in the front
         else if(list[0][sortKey] > insertItem[sortKey]){
             list.unshift(insertItem);
+            return list;
         } 
         
         //If the last item is less than the new item, put the item in the back
         else if(list[list.length - 1][sortKey] < insertItem[sortKey]){
-            
+            list.push(insertItem);
+            return list;
         }
+
+        //It's somewhere in the middle
+        else {
+            let left = 0;
+            let right = list.length - 1;
+            while(left < right){
+                let middle = (left + right) / 2;
+
+                //If the current middle is greater
+                if(list[middle][sortKey] > insertItem[sortKey]){
+                    //If the one below is lesser, insert between
+                    if(list[middle - 1][sortKey] < insertItem[sortKey]){
+                        list.splice(middle, 0, insertItem);
+                        return list;
+                    }
+                    
+                    //Otherwise, move earlier in the list
+                    else {
+                        right = middle - 1;
+                    }
+                }
+
+                //If the current middle is lesser
+                else {
+                    //If the next item is greater, insert between
+                    if(list[middle + 1][sortKey] > insertItem[sortKey]){
+                        list.splice(middle + 1, 0, insertItem);
+                        return list;
+                    }
+
+                    //Otherwise, move to later in the list
+                    else {
+                        left = middle + 1;
+                    }
+                }
+            }
+        }
+
+        //If it couldn't be inserted, return false
+        return false;
     },
 
     //Check if a name is valid
