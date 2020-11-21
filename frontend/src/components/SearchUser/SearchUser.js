4b9d2c785bdd8fe2de1fc5ff4b7a53d8afca5aa3
James Bradbury <fe4034@wayne.edu>
2020-11-21 18:22:28 -0500

    Fixed buttons for Add Friend, added all functionality for Friend requests, reject, confirm, cancel, send, and accept.

--- frontend/src/components/SearchUser/SearchUser.js
+++ frontend/src/components/SearchUser/SearchUser.js
@@ -15,6 +15,40 @@ const SearchUser=(props)=>{
   const [allUsers, setAllUsers] = useState([]);
   const [homeRender, setHomeRender] = useContext(HomeRenderContext);
   const [search, setSearch] = useState('');
+  const [friendReqSent, setfriendReqSent] = useState([]);
+
+  function shallowEqual(object1, object2) {
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+  
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+  
+    for (let key of keys1) {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  console.log(friendReqSent);
+  console.log(user);
+
+  useEffect(()=>{
+    setfriendReqSent(user.FriendRequests);
+  }, [user])
+
+  useEffect(() => {
+    if (!cookies.get("Token"))
+      props.history.push("/login");
+    getAllUsers();
+    if (user.ID !== "") {
+      setfriendReqSent(user.FriendRequests);
+    }
+  }, [])
+  
   
   const getAllUsers = () => {
     const link = `${REACT_APP_URL}Accounts`;
@@ -26,6 +60,103 @@ const SearchUser=(props)=>{
       })
       .catch((error) => console.log(error))
   }
+
+  const addFriend=(ID) =>{
+    const headers = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const Link = `${process.env.REACT_APP_URL}AddFriend/Account/${user.ID}`;
+    const payload={
+        Requested: ID
+    }
+    axios
+    .patch(Link, payload, headers)
+    .then((res) => {
+      console.log("patch res: ", res.data);
+    })
+    .catch((error) => console.log(error));
+  }
+
+  const confirmFriend=(ID) =>{
+    const headers = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const Link = `${process.env.REACT_APP_URL}AddFriend/Account/${user.ID}`;
+    const payload={
+        Confirm: ID
+    }
+    axios
+    .patch(Link, payload, headers)
+    .then((res) => {
+      console.log("patch res: ", res.data);
+    })
+    .catch((error) => console.log(error));
+  }
+  
+  const removeFriend=(ID) =>{
+    const headers = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const Link = `${process.env.REACT_APP_URL}AddFriend/Account/${user.ID}`;
+    const payload={
+        Remove: ID
+    }
+    axios
+    .patch(Link, payload, headers)
+    .then((res) => {
+      console.log("patch res: ", res.data);
+    })
+    .catch((error) => console.log(error));
+  }
+  
+  const undoFriend=(ID) =>{
+    const headers = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const Link = `${process.env.REACT_APP_URL}AddFriend/Account/${user.ID}`;
+    const payload={
+        RemoveRequest: ID
+    }
+    axios
+    .patch(Link, payload, headers)
+    .then((res) => {
+      console.log("patch res: ", res.data);
+    })
+    .catch((error) => console.log(error));
+  }
+
+  const renderButtons = (pid, username) => {
+    if (friendReqSent) {
+      if (friendReqSent.some(att => att.ID.includes(pid))) {
+        if (friendReqSent.some(att => shallowEqual(att, {ID:pid, Sender:true, Username:username})))
+          return <Button variant="outline-success" onClick={() => undoFriend(pid)}>Request Sent</Button>
+        else
+        {
+          return <Button variant="outline-success" onClick={() => confirmFriend(pid)} >Accept Request</Button>
+        }
+      }
+      else
+        return <Button variant="outline-primary" onClick={() => addFriend(pid)}>Add Friend</Button>
+    }
+    else
+      return <p>empty</p>
+  }
+
+  const renderEXButton = (pid, username) => {
+     if (friendReqSent.some(att => shallowEqual(att, {ID:pid, Sender:false, Username:username})))
+      return <Button variant="outline-success" onClick={() => confirmFriend(pid)} >Reject Request</Button> 
+    else
+      return <p></p>
+  }
+  
 
   useEffect(() => {
     getAllUsers();
@@ -41,9 +172,7 @@ const SearchUser=(props)=>{
 
   return(
     <div>
-    
     <input type= "text" placeholder = "search users" onChange = { e => setSearch(e.target.value)} />
-
       <Accordion>
         {filteredUsers.map((p, i) => (
           <Card>
@@ -60,17 +189,17 @@ const SearchUser=(props)=>{
             </Accordion.Toggle>
             <Accordion.Collapse eventKey={p.ID}>
               <Card.Body>
-                
-                <div className="searchUser-buttons">
+              <div className="searchUser-buttons">
                   <div className="settings-accordian-buttons">
                     <Button variant="outline-danger">Report</Button>{' '}
                     <div class="divider"/>
                     <Button variant="outline-secondary">Block</Button>{' '}
                     <div class="divider"/>
-                    <Button variant="outline-primary">Add Friend</Button>{' '}
+                    {renderButtons(p.ID, p.Username)}
+                    <div class="divider"/>
+                    {renderEXButton(p.ID, p.Username)}
                   </div>
                 </div>
-                
               </Card.Body>
             </Accordion.Collapse>
           </Card>
