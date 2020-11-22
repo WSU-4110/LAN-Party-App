1d655616843ca977a37c873385d32102ec6caa82
James Bradbury <fe4034@wayne.edu>
2020-11-21 19:51:22 -0500

    home.js correction

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -85,7 +85,9 @@ const Home = (props) => {
             eventKey={p.ID}>
               {p.PartyName} <br/>
               Host: {p.HostUsername} <br/>
-              Location: {p.Location.hasOwnProperty(Name) ? p.Location.Name : p.Location} 
+              Location: {p.PartyLocation.hasOwnProperty("Name") ? 
+              p.PartyLocation.Name 
+              : p.PartyLocation} <br/>
               Date: {p.PartyTime} <br/>
             </Accordion.Toggle>
             <Accordion.Collapse eventKey={p.ID}>
